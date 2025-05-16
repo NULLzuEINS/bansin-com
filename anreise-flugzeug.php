c50acbc7cf31a21409d3831054e51611fcdb0828
André Lademann <alademann@thinkport.digital>
2025-05-17 00:02:04 +0200

    fix(anreise-flugzeug.php): correct special characters in German text for better readability and accuracy
    style(anreise-schiff.php): correct HTML structure and improve readability
    
    Refactor HTML to fix misaligned tags and improve code clarity. Adjust
    alt text for images to enhance accessibility.

--- anreise-flugzeug.php
+++ anreise-flugzeug.php
@@ -1,124 +1,124 @@
- <?php include "inc/header.inc.php"; ?>
- <?php include "inc/image.inc.php"; ?>
+<?php include "inc/header.inc.php"; ?>
+<?php include "inc/image.inc.php"; ?>
 
- <table cellspacing="0" cellpadding="0" width="100%" border="0">
-     <tbody>
-         <tr>
-             <td align="left" valign="top">
-                 <table cellspacing="0" cellpadding="0" width="100%" border="0">
-                     <tbody>
-                         <tr bgcolor="#ffffff">
-                             <td height="41" colspan="2" valign="top">
-                                 <font face="Verdana, Arial, Helvetica!, sans-serif" size="2">
-                                     <b><span class="ueberschrift">Anreisen mit System!</span></b>
-                                 </font>
-                             </td>
-                         </tr>
-                         <tr bgcolor="#ffffff">
-                             <td valign="top" width="55%" height="260">
-                                 <hr size="1" color="#f0f0f0" class="trenner">
-                                 <table border="0" width="435" height="12" bgcolor="#3399cc" cellpadding="0" cellspacing="0">
-                                     <tr align="center">
-                                         <td height="10"><a href="anreise-auto.php">
-                                                 <font color="#ffffff">Auto</font>
-                                             </a></td>
-                                         <td height="10"><a href="anreise-bahn.php">
-                                                 <font color="#ffffff">Bahn</font>
-                                             </a>
-                                         <td height="10"><a href="anreise-bus.php">
-                                                 <font color="#ffffff">Bus</font>
-                                             </a></td>
-                                         <td height="10"><a href="anreise-flugzeug.php">
-                                                 <font color="#ffffff">Flugzeug</font>
-                                             </a></td>
-                                         <td height="10"><a href="anreise-schiff.php">
-                                                 <font color="#ffffff">Schiff</font>
-                                             </a></td>
-                                     <tr>
-                                 </table>
-                                 <br> <span class="inhalt"> <img src="bilder/flugzeug.jpg" alt="" hspace="5" border="0" align="left">
-                                     In Zirchow befindet sich der regionale
-                                     <a href="http://www.flughafen-heringsdorf.de/" target="insel">Flughafen
-                                         Heringsdorf</a>, der einzige Flughafen Vorpommerns mit Linienverkehr. Die Lï¿½nge der
-                                     Landebahn betrï¿½gt 2300m.
-                                     <br>Hier kï¿½nnen Sie bei uns landen.</span><br>
-                                 <br><br>
-
-
-
-
-
-                                 <br><br>
-                                 <!-- AUTO HIMMELSRICHTUNG -->
-
-                                 <table border="0" cellpadding="0" cellspacing="0" width="435">
-                                     <tr>
-                                         <td width="435" bgcolor="#3399cc" class="headlines">
-                                             <font color="#ffffff">Flughafen Zirchow</font>
-                                         </td>
-                                     </tr>
-                                 </table>
-
-
-                                 <table cellspacing="0" cellpadding="5" width="435" bgcolor="#f0f0f0">
-
-                                     <tr bgcolor="#f0f0f0">
-                                         <td width="350" bgcolor="#f0f0f0" class="erlaeuterung">
-
-
-                                             <ul>
-                                                 <li><b>Linienverbindungen</b><br>
-                                                     <ul>
-                                                         <li>
-                                                             Im Sommer besteht zwischen D&uuml;sseldorf und der Insel ein Linienverkehr( Linienflï¿½ge 2003).<br>
-                                                             Zubringerflï¿½ge zum Flughafen D&uuml;sseldorf von <br>Mï¿½nchen, Stuttgart, London (STN), Paris (CDG),Zï¿½rich.<br>
-                                                         <li>Ab Mai mit der CIRRUS Airline von Mannheim, Saarbrï¿½cken oder Karlsruhe/Baden-Baden ï¿½ber Berlin auf die Insel Usedom
-                                                             <br>
-                                                             Die CIRRUS Airline wird die Flugverbindung von Mannheim nach Berlin, von
-                                                             Saarbrï¿½cken nach Berlin und von&nbsp;<br>
-                                                             Karlsruhe/Baden-Baden nach Berlin an zwei Tagen in der Woche auf die Insel
-                                                             Usedom verlï¿½ngern.<br>
-                                                             Als Flugtage sind der Samstag und der Dienstag vorgesehen.
-                                                     </ul>
-                                                 <li><b>Bedarfscharter</b><br>Bedarfslinienflï¿½ge <br>
-                                                     Berlin- THF, Gï¿½ttin/Rï¿½gen, Gï¿½ttin/Rundflug<br>
-                                                     Auch Charterfl&uuml;ge sind m&ouml;glich. <br>
-                                                 <li><b>Rundfl&uuml;ge</b><br>
-                                                     Rundfl&uuml;ge &uuml;ber die Insel und Umgebung.
-                                             </ul><br>
-                                             <ul>
-                                                 <li><b>Privater Flugverkehr</b><br>
-                                                     Hier k&ouml;nnen Sportflugzeuge starten und landen.
-                                             </ul>
-                                             <ul>Auf dem Flugplatz in Peenem&uuml;nde mit einer
-                                                 Landebahn von 1300 m sind nur Sportflieger zugelassen.
-                                             </ul>
-                                         </td>
-                                     </tr>
-                                 </table>
+<table cellspacing="0" cellpadding="0" width="100%" border="0">
+    <tbody>
+        <tr>
+            <td align="left" valign="top">
+                <table cellspacing="0" cellpadding="0" width="100%" border="0">
+                    <tbody>
+                        <tr bgcolor="#ffffff">
+                            <td height="41" colspan="2" valign="top">
+                                <font face="Verdana, Arial, Helvetica!, sans-serif" size="2">
+                                    <b><span class="ueberschrift">Anreisen mit System!</span></b>
+                                </font>
+                            </td>
+                        </tr>
+                        <tr bgcolor="#ffffff">
+                            <td valign="top" width="55%" height="260">
+                                <hr size="1" color="#f0f0f0" class="trenner">
+                                <table border="0" width="435" height="12" bgcolor="#3399cc" cellpadding="0" cellspacing="0">
+                                    <tr align="center">
+                                        <td height="10"><a href="anreise-auto.php">
+                                                <font color="#ffffff">Auto</font>
+                                            </a></td>
+                                        <td height="10"><a href="anreise-bahn.php">
+                                                <font color="#ffffff">Bahn</font>
+                                            </a>
+                                        <td height="10"><a href="anreise-bus.php">
+                                                <font color="#ffffff">Bus</font>
+                                            </a></td>
+                                        <td height="10"><a href="anreise-flugzeug.php">
+                                                <font color="#ffffff">Flugzeug</font>
+                                            </a></td>
+                                        <td height="10"><a href="anreise-schiff.php">
+                                                <font color="#ffffff">Schiff</font>
+                                            </a></td>
+                                    <tr>
+                                </table>
+                                <br> <span class="inhalt"> <img src="bilder/flugzeug.jpg" alt="" hspace="5" border="0" align="left">
+                                    In Zirchow befindet sich der regionale
+                                    <a href="http://www.flughafen-heringsdorf.de/" target="insel">Flughafen
+                                        Heringsdorf</a>, der einzige Flughafen Vorpommerns mit Linienverkehr. Die Länge der
+                                    Landebahn beträgt 2300m.
+                                    <br>Hier können Sie bei uns landen.</span><br>
+                                <br><br>
 
 
 
 
-                             </td>
-                             <td valign="top" width="45%">
-                                 <div align="right">
-                                     <br><br><a href="#" onclick="fenster('bilder/karte-usedom.gif','Usedom','Beachten Sie bei der Fahrt nach Rï¿½gen bitte die Brï¿½ckenï¿½ffnungszeiten.')"><img src="bilder/karte-usedom.gif" alt="Ihre Ferienwohnung ist rot markiert" width="100" hspace="5" border="0"></a><br>
-                                     <br>
-                                     <a href="#" onclick="fenster('bilder/karte-bansin.gif','Usedom ï¿½bersicht','Haben Sie Bansin einmal erreicht ist es ein Kinderspiel zur Villa Cosima zu finden. Diese Karte soll Ihnen dabei helfen.')"></p>
-                                         <img src="bilder/karte-bansin.gif" alt="kompfortale Sitzgelegenheit an gro&szlig;en Fenstern" width="100" hspace="5" border="0"></a></p>
 
-                                 </div>
-                             </td>
+                                <br><br>
+                                <!-- AUTO HIMMELSRICHTUNG -->
 
-                         </tr>
-                     </tbody>
-                 </table>
-
-             </td>
-         </tr>
-     </tbody>
- </table>
+                                <table border="0" cellpadding="0" cellspacing="0" width="435">
+                                    <tr>
+                                        <td width="435" bgcolor="#3399cc" class="headlines">
+                                            <font color="#ffffff">Flughafen Zirchow</font>
+                                        </td>
+                                    </tr>
+                                </table>
 
 
- <?php include "inc/footer.inc.php"; ?>
+                                <table cellspacing="0" cellpadding="5" width="435" bgcolor="#f0f0f0">
+
+                                    <tr bgcolor="#f0f0f0">
+                                        <td width="350" bgcolor="#f0f0f0" class="erlaeuterung">
+
+
+                                            <ul>
+                                                <li><b>Linienverbindungen</b><br>
+                                                    <ul>
+                                                        <li>
+                                                            Im Sommer besteht zwischen Düsseldorf und der Insel ein Linienverkehr (Linienflüge 2003).<br>
+                                                            Zubringerflüge zum Flughafen Düsseldorf von <br>München, Stuttgart, London (STN), Paris (CDG), Zürich.<br>
+                                                        <li>Ab Mai mit der CIRRUS Airline von Mannheim, Saarbrücken oder Karlsruhe/Baden-Baden über Berlin auf die Insel Usedom
+                                                            <br>
+                                                            Die CIRRUS Airline wird die Flugverbindung von Mannheim nach Berlin, von
+                                                            Saarbrücken nach Berlin und von&nbsp;<br>
+                                                            Karlsruhe/Baden-Baden nach Berlin an zwei Tagen in der Woche auf die Insel
+                                                            Usedom verlängern.<br>
+                                                            Als Flugtage sind der Samstag und der Dienstag vorgesehen.
+                                                    </ul>
+                                                <li><b>Bedarfscharter</b><br>Bedarfslinienflüge <br>
+                                                    Berlin- THF, Göttin/Rügen, Göttin/Rundflug<br>
+                                                    Auch Charterflüge sind möglich. <br>
+                                                <li><b>Rundflüge</b><br>
+                                                    Rundflüge über die Insel und Umgebung.
+                                            </ul><br>
+                                            <ul>
+                                                <li><b>Privater Flugverkehr</b><br>
+                                                    Hier können Sportflugzeuge starten und landen.
+                                            </ul>
+                                            <ul>Auf dem Flugplatz in Peenemünde mit einer
+                                                Landebahn von 1300 m sind nur Sportflieger zugelassen.
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </table>
+
+
+
+
+                            </td>
+                            <td valign="top" width="45%">
+                                <div align="right">
+                                    <br><br><a href="#" onclick="fenster('bilder/karte-usedom.gif','Usedom','Beachten Sie bei der Fahrt nach Rügen bitte die Brückenöffnungszeiten.')"><img src="bilder/karte-usedom.gif" alt="Ihre Ferienwohnung ist rot markiert" width="100" hspace="5" border="0"></a><br>
+                                    <br>
+                                    <a href="#" onclick="fenster('bilder/karte-bansin.gif','Usedom Übersicht','Haben Sie Bansin einmal erreicht ist es ein Kinderspiel zur Villa Cosima zu finden. Diese Karte soll Ihnen dabei helfen.')"></p>
+                                        <img src="bilder/karte-bansin.gif" alt="kompfortale Sitzgelegenheit an großen Fenstern" width="100" hspace="5" border="0"></a></p>
+
+                                </div>
+                            </td>
+
+                        </tr>
+                    </tbody>
+                </table>
+
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+
+<?php include "inc/footer.inc.php"; ?>
--- anreise-schiff.php
+++ anreise-schiff.php
@@ -1,74 +1,65 @@
- <?php include "inc/header.inc.php"; ?>
- <?php include "inc/image.inc.php"; ?>
- <table cellspacing="0" cellpadding="0" width="100%" border="0">
-     <tbody>
-         <tr>
-             <td align="left" valign="top">
-                 <table cellspacing="0" cellpadding="0" width="100%" border="0">
-                     <tbody>
-                         <tr bgcolor="#ffffff">
-                             <td height="41" colspan="2" valign="top">
-                                 <font face="Verdana, Arial, Helvetica!, sans-serif" size="2">
-                                     <b><span class="ueberschrift">Anreisen mit System!</span></b>
-                                 </font>
-                             </td>
-                         </tr>
-                         <tr bgcolor="#ffffff">
-                             <td valign="top" width="55%" height="260">
-                                 <hr size="1" color="#f0f0f0" class="trenner">
-                                 <table border="0" width="435" height="12" bgcolor="#3399cc" cellpadding="0" cellspacing="0">
-                                     <tr align="center">
-                                         <td height="10"><a href="anreise-auto.php">
-                                                 <font color="#ffffff">Auto</font>
-                                             </a></td>
-                                         <td height="10"><a href="anreise-bahn.php">
-                                                 <font color="#ffffff">Bahn</font>
-                                             </a>
-                                         <td height="10"><a href="anreise-bus.php">
-                                                 <font color="#ffffff">Bus</font>
-                                             </a></td>
-                                         <td height="10"><a href="anreise-flugzeug.php">
-                                                 <font color="#ffffff">Flugzeug</font>
-                                             </a></td>
-                                         <td height="10"><a href="anreise-schiff.php">
-                                                 <font color="#ffffff">Schiff</font>
-                                             </a></td>
-                                     <tr>
-                                 </table>
-                                 <br> <span class="inhalt">
-                                     <img src="bilder/schiff.jpg" alt="" hspace="5" border="0" align="left">
-                                     Wenn Sie sich der Insel vom Wasser her n&auml;hern,
-                                     bietet Usedom an Achterwasser und Peenestrom zahlreiche Pl&auml;tze zum Ankern.
-                                 </span><br><br><br>
-                                 <br><br>
+<?php include "inc/header.inc.php"; ?>
+<?php include "inc/image.inc.php"; ?>
+<table cellspacing="0" cellpadding="0" width="100%" border="0">
+    <tbody>
+        <tr>
+            <td align="left" valign="top">
+                <table cellspacing="0" cellpadding="0" width="100%" border="0">
+                    <tbody>
+                        <tr bgcolor="#ffffff">
+                            <td height="41" colspan="2" valign="top">
+                                <font face="Verdana, Arial, Helvetica, sans-serif" size="2">
+                                    <b><span class="ueberschrift">Anreisen mit System!</span></b>
+                                </font>
+                            </td>
+                        </tr>
+                        <tr bgcolor="#ffffff">
+                            <td valign="top" width="55%" height="260">
+                                <hr size="1" color="#f0f0f0" class="trenner">
+                                <table border="0" width="435" height="12" bgcolor="#3399cc" cellpadding="0" cellspacing="0">
+                                    <tr align="center">
+                                        <td height="10"><a href="anreise-auto.php">
+                                                <font color="#ffffff">Auto</font>
+                                            </a></td>
+                                        <td height="10"><a href="anreise-bahn.php">
+                                                <font color="#ffffff">Bahn</font>
+                                            </a></td>
+                                        <td height="10"><a href="anreise-bus.php">
+                                                <font color="#ffffff">Bus</font>
+                                            </a></td>
+                                        <td height="10"><a href="anreise-flugzeug.php">
+                                                <font color="#ffffff">Flugzeug</font>
+                                            </a></td>
+                                        <td height="10"><a href="anreise-schiff.php">
+                                                <font color="#ffffff">Schiff</font>
+                                            </a></td>
+                                    </tr>
+                                </table>
+                                <br>
+                                <span class="inhalt">
+                                    <img src="bilder/schiff.jpg" alt="Schiff auf dem Wasser" hspace="5" border="0" align="left">
+                                    Wenn Sie sich der Insel vom Wasser her nähern,
+                                    bietet Usedom an Achterwasser und Peenestrom zahlreiche Plätze zum Ankern.
+                                </span><br>
+                            </td>
+                            <td valign="top" width="45%">
+                                <div align="right">
+                                    <br><br>
+                                    <a href="#" onclick="fenster('bilder/karte-usedom.gif','Usedom','Beachten Sie bei der Fahrt nach Rügen bitte die Brückenöffnungszeiten.')">
+                                        <img src="bilder/karte-usedom.gif" alt="Ihre Ferienwohnung ist rot markiert" width="100" hspace="5" border="0">
+                                    </a><br>
+                                    <br>
+                                    <a href="#" onclick="fenster('bilder/karte-bansin.gif','Usedom Übersicht','Haben Sie Bansin einmal erreicht ist es ein Kinderspiel zur Villa Cosima zu finden. Diese Karte soll Ihnen dabei helfen.')">
+                                        <img src="bilder/karte-bansin.gif" alt="komfortable Sitzgelegenheit an großen Fenstern" width="100" hspace="5" border="0">
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
-
-
-
-
-
-
-
-
-                             </td>
-                             <td valign="top" width="45%">
-                                 <div align="right">
-                                     <br><br><a href="#" onclick="fenster('bilder/karte-usedom.gif','Usedom','Beachten Sie bei der Fahrt nach Rï¿½gen bitte die Brï¿½ckenï¿½ffnungszeiten.')"><img src="bilder/karte-usedom.gif" alt="Ihre Ferienwohnung ist rot markiert" width="100" hspace="5" border="0"></a><br>
-                                     <br>
-                                     <a href="#" onclick="fenster('bilder/karte-bansin.gif','Usedom ï¿½bersicht','Haben Sie Bansin einmal erreicht ist es ein Kinderspiel zur Villa Cosima zu finden. Diese Karte soll Ihnen dabei helfen.')"></p>
-                                         <img src="bilder/karte-bansin.gif" alt="kompfortale Sitzgelegenheit an gro&szlig;en Fenstern" width="100" hspace="5" border="0"></a></p>
-
-                                 </div>
-                             </td>
-
-                         </tr>
-                     </tbody>
-                 </table>
-
-             </td>
-         </tr>
-     </tbody>
- </table>
-
-
- <?php include "inc/footer.inc.php"; ?>
+<?php include "inc/footer.inc.php"; ?>
