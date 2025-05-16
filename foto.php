bf3d981b3d6a2ed489eb2840c87115a9bd16f838
André Lademann <alademann@thinkport.digital>
2025-05-17 00:20:46 +0200

    refactor(foto.php): replace table-based layout with div for thumbnails style(foto.php): add CSS for horizontal scrolling of thumbnails feat(foto.php): implement JavaScript to update main image on thumbnail click

--- foto.php
+++ foto.php
@@ -53,67 +53,20 @@
                             <!--
                             -->
                         </script>
-                        <table border="0" cellspacing="0" cellpadding="5" width="465">
-                            <tr>
-                                <td nowrap="" align="center">
-                                    <layer visibility="hide">
-                                        <img border="0" align="middle" src="photogallery/photo4023/prevdis.gif" lowsrc="photogallery/photo4023/prev.gif" id="fpGalleryLeftBtn_516" onclick="JavaScript1.1:fp_ScrollLeft(516)" width="25" height="29" />
-                                    </layer>
-                                    <script language="JavaScript1.1">
-                                        <!--
-                                        if (fp_ie4()) {
-                                            document.write("<span align='center' style='width:465;overflow:hidden' id='fpGalleryListCell_516'>");
-                                        }
-                                        if (fp_ns6()) {
-                                            document.getElementById("fpGalleryLeftBtn_516").style.visibility = "hidden"
-                                        }
-                                        -->
-                                    </script>
-                                    <a target="_self" href="Javascript:fp_ShowImg(document['fpphoto_8131'],'435','326','516',0);"><img hspace="10" vspace="5" border="0" src="photogallery/photo4023/villacosima.jpg" id="fpphoto_8131" name="fpphoto_8131" lowsrc="bilder/ansicht/villacosima.jpg" width="90" height="68" title="Die Villa Cosima" align="absmiddle" /></a>
-                                    <a target="_self" href="Javascript:fp_ShowImg(document['fpphoto_2232'],'435','326','516',1);"><img hspace="10" vspace="5" border="0" src="photogallery/photo4023/kueche.jpg" id="fpphoto_2232" name="fpphoto_2232" lowsrc="bilder/ansicht/kueche.jpg" width="90" height="68" title="Die Küchenzeile" align="absmiddle" /></a>
-                                    <a target="_self" href="Javascript:fp_ShowImg(document['fpphoto_1116'],'435','326','516',2);"><img hspace="10" vspace="5" border="0" src="photogallery/photo4023/sitzgelegenheit.jpg" id="fpphoto_1116" name="fpphoto_1116" lowsrc="bilder/ansicht/sitzgelegenheit.jpg" width="90" height="68" title="Die Sitzgelegenheit" align="absmiddle" /></a>
-                                    <a target="_self" href="Javascript:fp_ShowImg(document['fpphoto_1810'],'435','326','516',3);"><img hspace="10" vspace="5" border="0" src="photogallery/photo4023/sitzgelegenheiten.jpg" id="fpphoto_1810" name="fpphoto_1810" lowsrc="bilder/ansicht/sitzgelegenheiten.jpg" width="90" height="68" title="Die Sitzgelegenheit" align="absmiddle" /></a>
-                                    <a target="_self" href="Javascript:fp_ShowImg(document['fpphoto_2980'],'435','326','516',4);"><img hspace="10" vspace="5" border="0" src="photogallery/photo4023/wohnzimmerfenster.jpg" id="fpphoto_2980" name="fpphoto_2980" lowsrc="bilder/ansicht/wohnzimmerfenster.jpg" width="90" height="68" title="Das Wohnzimmer" align="absmiddle" /></a>
-                                    <script language="JavaScript1.1">
-                                        <!--
-                                        if (fp_ns6()) {
-                                            document.write("");
-                                        }
-                                        -->
-                                    </script>
-                                    <a target="_self" href="Javascript:fp_ShowImg(document['fpphoto_5056'],'435','326','516',5);"><img hspace="10" vspace="5" border="0" src="photogallery/photo4023/wohnzimmertisch.jpg" id="fpphoto_5056" name="fpphoto_5056" lowsrc="bilder/ansicht/wohnzimmertisch.jpg" width="90" height="68" title="Das Wohnzimmer" align="absmiddle" /></a>
-                                    <a target="_self" href="Javascript:fp_ShowImg(document['fpphoto_4317'],'435','326','516',6);"><img hspace="10" vspace="5" border="0" src="photogallery/photo4023/schlafzimmer.jpg" id="fpphoto_4317" name="fpphoto_4317" lowsrc="bilder/ansicht/schlafzimmer.jpg" width="90" height="68" title="Das Schlafzimmer" align="absmiddle" /></a>
-                                    <a target="_self" href="Javascript:fp_ShowImg(document['fpphoto_6329'],'435','580','516',7);"><img hspace="10" vspace="5" border="0" src="photogallery/photo4023/bad.jpg" id="fpphoto_6329" name="fpphoto_6329" lowsrc="bilder/ansicht/bad.jpg" width="90" height="68" title="Das Badezimmer" align="absmiddle" /></a>
-                                    <span style="width:0;height:68;visibility:hidden">.</span>
-                                    <script language="JavaScript1.1">
-                                        <!--
-                                        if (fp_ie4()) {
-                                            document.write("</span>");
-                                        }
-                                        -->
-                                    </script>
-                                    <layer visibility="hide">
-                                        <img border="0" align="middle" src="photogallery/photo4023/next.gif" lowsrc="photogallery/photo4023/nextdis.gif" id="fpGalleryRightBtn_516" onclick="JavaScript1.1:fp_ScrollRight(516)" width="25" height="29" />
-                                        <script language="JavaScript1.1">
-                                            rightdisabled = false
-                                            widthLength = 570
-                                            if (fp_ns6()) {
-                                                document.getElementById("fpGalleryRightBtn_516").style.visibility = "hidden"
-                                            }
-                                        </script>
-                                    </layer>
-                                    <hr style="height:1" />
-                                </td>
-                            </tr>
-                        </table>
+                        <div class="thumbnails-scroll">
+                            <img hspace="10" vspace="5" border="0" src="photogallery/photo4023/villacosima.jpg" width="90" height="68" title="Die Villa Cosima" data-large="bilder/ansicht/villacosima.jpg" />
+                            <img hspace="10" vspace="5" border="0" src="photogallery/photo4023/kueche.jpg" width="90" height="68" title="Die Küchenzeile" data-large="bilder/ansicht/kueche.jpg" />
+                            <img hspace="10" vspace="5" border="0" src="photogallery/photo4023/sitzgelegenheit.jpg" width="90" height="68" title="Die Sitzgelegenheit" data-large="bilder/ansicht/sitzgelegenheit.jpg" />
+                            <img hspace="10" vspace="5" border="0" src="photogallery/photo4023/sitzgelegenheiten.jpg" width="90" height="68" title="Die Sitzgelegenheit" data-large="bilder/ansicht/sitzgelegenheiten.jpg" />
+                            <img hspace="10" vspace="5" border="0" src="photogallery/photo4023/wohnzimmerfenster.jpg" width="90" height="68" title="Das Wohnzimmer" data-large="bilder/ansicht/wohnzimmerfenster.jpg" />
+                            <img hspace="10" vspace="5" border="0" src="photogallery/photo4023/wohnzimmertisch.jpg" width="90" height="68" title="Das Wohnzimmer" data-large="bilder/ansicht/wohnzimmertisch.jpg" />
+                            <img hspace="10" vspace="5" border="0" src="photogallery/photo4023/schlafzimmer.jpg" width="90" height="68" title="Das Schlafzimmer" data-large="bilder/ansicht/schlafzimmer.jpg" />
+                            <img hspace="10" vspace="5" border="0" src="photogallery/photo4023/bad.jpg" width="90" height="68" title="Das Badezimmer" data-large="bilder/ansicht/bad.jpg" />
+                        </div>
                         <img id="fpGalleryMainImg_516" name="fpGalleryMainImg_516" width="435" height="326" src="bilder/ansicht/villacosima.jpg" title="Die Villa Cosima" />
-                        <layer visibility="hide">
-                            <div id="fpGalleryCaptionCell_516">
-                                <p>Die Villa Cosima</p>
-                            </div>
-                            <div id="fpGalleryDescCell_516">
-                            </div>
-                        </layer>
+                        <div id="fpGalleryCaptionCell_516">
+                            <p>Die Villa Cosima</p>
+                        </div>
                     </center>
                 </div>
                 <!--webbot bot="PhotoAlbum" i-checksum="14565" endspan -->
@@ -121,5 +74,33 @@
         </tr>
     </tbody>
 </table>
+
+<style>
+.thumbnails-scroll {
+    overflow-x: auto;
+    white-space: nowrap;
+    width: 100%;
+    padding-bottom: 10px;
+}
+.thumbnails-scroll a {
+    display: inline-block;
+    vertical-align: middle;
+}
+.thumbnails-scroll img {
+    max-width: none;
+}
+</style>
+
+<script>
+document.querySelectorAll('.thumbnails-scroll img').forEach(function(thumb) {
+    thumb.addEventListener('click', function(e) {
+        var mainImg = document.getElementById('fpGalleryMainImg_516');
+        mainImg.src = this.getAttribute('data-large');
+        mainImg.title = this.title;
+        var caption = document.getElementById('fpGalleryCaptionCell_516');
+        if (caption) caption.innerHTML = '<p>' + this.title + '</p>';
+    });
+});
+</script>
 
 <?php include "inc/footer.inc.php"; ?>
